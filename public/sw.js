9ddf5dc8d56b30cd5929554cb7825db3d858af84
Rodrigo12851 <musuario688@gmail.com>
2026-08-05 20:15:49 -0300

    refactor(assets): migrate PWA icons to SVG
    Replace raster icon files with optimized SVG assets for better scalability and reduced bundle size. Update manifest and service worker references accordingly.

--- public/sw.js
+++ public/sw.js
@@ -4,10 +4,10 @@ const PRECACHE_URLS = [
   '/index.html',
   '/manifest.json',
   '/favicon.svg',
-  '/favicon-32x32.png',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/apple-touch-icon.png'
+  '/icon-192.svg',
+  '/icon-512.svg',
+  '/icon-maskable-192.svg',
+  '/icon-maskable-512.svg'
 ];
 
 // Installation phase: cache core shell files
